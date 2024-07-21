4a9c6c2082aa1da41dd0084a87eaee23624c42fa
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-07-21 11:39:27 +0200

    edit NavBar.js 'Added the basics code to the navbar structure' + edit index.css 'Added general css rules to the website elements'

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,34 @@
-import "../styles/NavBar.module.css";
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">TechPlaza</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+            <NavDropdown title="About" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">About Us</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Our Story</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Our Mission and Vision</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Why Choose Us</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">FAQ's</NavDropdown.Item>
             </NavDropdown>
+            <NavDropdown title="Services" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Our Applications</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">IT Support Service</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#link">Contact us</Nav.Link>
+            <Nav.Link href="#link">To Search</Nav.Link>
+            <Nav.Link href="#link">Sign in</Nav.Link>
+            <Nav.Link href="#link">Sign up</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
