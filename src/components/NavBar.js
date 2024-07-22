89fd1d5b2567f70eac781822089984bb2c7eecd8
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-07-22 15:29:21 +0200

    edit NavBar.js 'change in the Navbar structure' + edit NavBar.module.css 'edit some styles for Navbar and remove some classes'

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import styles from "../styles/NavBar.module.css";
 import Logo from "../assets/logo.png";
 
@@ -16,22 +15,13 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="m-auto">
-            <Nav.Link href="#home" className={styles.Nav_Anchor_Tag}>Home</Nav.Link>
-            <NavDropdown title={<span className={styles.Nav_Anchor_Tag}>About</span>} id="basic-nav-dropdown" className={styles.Nav_Anchor_Tag}>
-              <NavDropdown.Item href="#action/3.1" className={styles.Nav_Anchor_Tag}>About Us</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.1" className={styles.Nav_Anchor_Tag}>Our Story</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.1" className={styles.Nav_Anchor_Tag}>Our Mission and Vision</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.1" className={styles.Nav_Anchor_Tag}>Why Choose Us</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.1" className={styles.Nav_Anchor_Tag}>FAQ's</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title={<span className={styles.Nav_Anchor_Tag}>Services</span>} id="basic-nav-dropdown" className={styles.Nav_Anchor_Tag}>
-              <NavDropdown.Item href="#action/3.1" className={styles.Nav_Anchor_Tag}>Our Applications</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.1" className={styles.Nav_Anchor_Tag}>IT Support Service</NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#home" className={`${styles.Nav_Anchor_Tag} ${styles.Active}`}>Home</Nav.Link>
+            <Nav.Link href="#home" className={`${styles.Nav_Anchor_Tag}`}>About Us</Nav.Link>
+            <Nav.Link href="#home" className={`${styles.Nav_Anchor_Tag}`}>Our Services</Nav.Link>
             <Nav.Link href="#link" className={styles.Nav_Anchor_Tag}>Contact us</Nav.Link>
             <Nav.Link href="#link">To Search</Nav.Link>
-            <Nav.Link href="#link" className={styles.Nav_Anchor_Tag}>Sign in</Nav.Link>
-            <Nav.Link href="#link" className={styles.Nav_Anchor_Tag}>Sign up</Nav.Link>
+            <Nav.Link href="#link" className={`${styles.Nav_Anchor_Tag}`}>Sign in</Nav.Link>
+            <Nav.Link href="#link" className={`${styles.Nav_Anchor_Tag}` }><button className={styles.NavButton}>Register  <i class="fa-solid fa-user-plus"></i></button></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
