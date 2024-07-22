a327bf41cb98f9bfbac934ddc50eb434ca7d2a85
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-07-23 01:28:10 +0200

    edit Home.js 'used react-typed + edit in the structure of the landing section' + edit Home.module.css 'add some styles for landing section' + update Json packages files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import { ReactTyped } from "react-typed";
 import styles from "../styles/Home.module.css";
 
 
 const Home = () => {
     return (
         <header className={styles.Landing}>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-md-6 col-lg-6'>
-                        <h1>We provide you Technology and Applications Solutions</h1>
-                        <h5>Try our Solutions and make your Life Easier</h5>
-                        <button className={styles.BtnSignup}><a href='#'>Register</a></button>
-                    </div>
-                </div>
+            <div className={styles.HeaderTypography}>
+                <h1>Techplaza is Your Partner in Innovative Application Solutions and Comprehensive IT Support</h1>
+                <h2>
+                    <ReactTyped
+                        className={styles.HeaderLoopText}
+                        strings={["Software Technical Support", "Hardware Technical Support", "Community Chat Rooms", "Work Events", "Community Applications"]}
+                        typeSpeed={70}
+                        backSpeed={50}
+                        showCursor={false}
+                        loop
+                    />
+                </h2>
             </div>
         </header>
     )
