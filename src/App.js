61311b9727d4131c8b1dd203d5e3a3a45449c1cb
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-07-23 14:50:11 +0200

    edit App.js 'remove NavBar.js compnent' + edit Home.js 'call NavBar.js component'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Fragment } from 'react';
 import './App.module.css';
-import NavBar from './components/NavBar';
 import Home from './pages/Home';
 
 function App() {
   return (
     <Fragment>
-      <NavBar />
       <Home />
     </Fragment>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { ReactTyped } from "react-typed";
+import NavBar from '../components/NavBar';
 import styles from "../styles/Home.module.css";
 
 
 const Home = () => {
     return (
-        <header className={styles.Landing}>
-            <div className={styles.HeaderTypography}>
-                <h1>Techplaza is Your Partner in Innovative Application Solutions and Comprehensive IT Support</h1>
-                <h2>
-                    <ReactTyped
-                        className={styles.HeaderLoopText}
-                        strings={["Software Technical Support", "Hardware Technical Support", "Community Chat Rooms", "Work Events", "Community Applications"]}
-                        typeSpeed={70}
-                        backSpeed={50}
-                        showCursor={false}
-                        loop
-                    />
-                </h2>
-            </div>
-        </header>
+        <Fragment>
+            < NavBar />
+            <header className={styles.Landing}>
+                <div className={styles.HeaderTypography}>
+                    <h1>Techplaza is Your Partner in Innovative Application Solutions and Comprehensive IT Support</h1>
+                    <h2>
+                        <ReactTyped
+                            className={styles.HeaderLoopText}
+                            strings={["Software Technical Support", "Hardware Technical Support", "Community Chat Rooms", "Work Events", "Community Applications"]}
+                            typeSpeed={50}
+                            backSpeed={40}
+                            showCursor={false}
+                            loop
+                        />
+                    </h2>
+                </div>
+            </header>
+        </Fragment>
     )
 }
 
