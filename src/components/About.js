c765283f281ecc7b1a3344cbd0fcca54a66969ea
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-07-23 23:38:24 +0200

    edit Home.js file 'remove the About us section structure' + edit Home.module.css 'remove the style for the about us section' + create new component under name About.js and copy the code inside it from the Home.js file + create About.module.css file to set style for about us section in home page

---
+++ src/components/About.js
@@ -0,0 +1,39 @@
+import React, { Fragment } from 'react'
+import styles from "../styles/About.module.css";
+import homeImageOne from "../assets/homeone.jpg"
+import homeImageTwo from "../assets/hometwo.jpg"
+
+const About = () => {
+    return (
+        <Fragment>
+            <section className={styles.HomeStaticPosts}>
+                <h2>About Us</h2>
+                <div className='container'>
+                    <div className='row justify-content-center align-items-center'>
+                        <div className='col-md-6 order-sm-1'>
+                            <img src={homeImageOne} alt='IT community' />
+                        </div>
+                        <div className='col-md-6 order-sm-2 d-flex justify-content-center align-items-center'>
+                            <h6>TechPlaza community is a vibrant and dynamic network of professionals, enthusiasts, and learners who are passionate about technology and its potential to transform our world. We are a diverse group of individuals from various backgrounds, experiences, and skill levels, united by our shared interest in information technology.
+                                We believe in the power of collaboration and mutual support. Our community members actively engage in knowledge sharing, problem-solving, and mentoring.</h6>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className={styles.HomeStaticPosts}>
+                <div className='container'>
+                    <div className='row justify-content-center align-items-center'>
+                        <div className='col-md-6 order-md-2 col-12 order-1'>
+                            <img src={homeImageTwo} alt='Applications' />
+                        </div>
+                        <div className='col-md-6 order-md-1 col-12 order-2 d-flex justify-content-center align-items-center'>
+                            <h6>TechPlaza platform applications are designed with the user in mind. They are intuitive, reliable, and above all, useful. We believe that technology should be a tool that makes your life easier, not more complicated. That’s why we spend countless hours refining our applications to ensure they are as user-friendly as possible.</h6>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </Fragment>
+    )
+}
+
+export default About
