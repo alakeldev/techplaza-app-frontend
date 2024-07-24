1e4696fa167d7c7a6a159e52adcc16dccbb22640
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-07-24 17:06:48 +0200

    remove About.js from components dir and add it to the pages dir + edit NavBar.js 'fix error on console change class to className attr' + edit Home.js remove the code in the header to saperate component file Header.js inside components dir + create Header.module.css to set css style to the Header.js + edit Home.module.css 'delete all the css code inside it'

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { ReactTyped } from "react-typed";
+import styles from "../styles/Header.module.css"
+
+
+function Header({ title, typedStrings }) {
+    return (
+        <header className={styles.Landing}>
+            <div className={styles.HeaderTypography}>
+                <h1>{title}</h1>
+                <h2>
+                    <ReactTyped
+                        className={styles.HeaderLoopText}
+                        strings={typedStrings}
+                        typeSpeed={50}
+                        backSpeed={40}
+                        showCursor={false}
+                        loop
+                    />
+                </h2>
+            </div>
+        </header>
+    );
+}
+
+export default Header
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,16 @@
 import React, { Fragment } from 'react';
-import { ReactTyped } from "react-typed";
+import Header from '../components/Header';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
-import About from '../components/About'
-import styles from "../styles/Home.module.css";
 
 const Home = () => {
     return (
         <Fragment>
             < NavBar />
-            <header className={styles.Landing}>
-                <div className={styles.HeaderTypography}>
-                    <h1>Techplaza is Your Partner in Innovative Application Solutions and Comprehensive IT Support</h1>
-                    <h2>
-                        <ReactTyped
-                            className={styles.HeaderLoopText}
-                            strings={["Software Technical Support", "Hardware Technical Support", "Community Chat Rooms", "Work Events", "Community Applications"]}
-                            typeSpeed={50}
-                            backSpeed={40}
-                            showCursor={false}
-                            loop
-                        />
-                    </h2>
-                </div>
-            </header>
-            <About />
+            <Header
+                title="TECHPLAZA is Your Partner in Innovative Application Solutions and Comprehensive IT Support"
+                typedStrings={["Software Technical Support", "Hardware Technical Support", "Community Chat Rooms", "Work Events", "Community Applications"]}
+            />
             <Footer />
         </Fragment>
     )
