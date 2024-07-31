47b38a3c6b36d0f1b16f7899b059bf49e1acb659
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-07-31 22:52:08 +0200

    edit EmailVerify.js 'import axios & useNavigate & toastify + create handleSubmit that check on otp and email and return a response and navigate to login page if the verify is successful + catch errors + edit in the Form and add a new email field to use can input its email that related to that otp'

--- src/pages/auth/EmailVerify.js
+++ src/pages/auth/EmailVerify.js
@@ -3,23 +3,57 @@ import { Form, Button, Container } from 'react-bootstrap';
 import NavBar from '../../components/NavBar';
 import Footer from '../../components/Footer';
 import styles from '../../styles/EmailVerify.module.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const EmailVerify = () => {
+
+  const navigate = useNavigate()
   const [otp, setOtp] = useState("")
+  const [email, setEmail] = useState("")
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (otp && email) {
+      try {
+        const response = await axios.post("http://127.0.0.1:8000/api/auth/email-verify/", { 'otp': otp, 'email': email });
+        if (response.status === 200) {
+          navigate("/login");
+          toast.success(response.data.message);
+        }
+      } catch (error) {
+        console.error("There was an error verifying the OTP:", error);
+        toast.error("Failed to verify OTP. Please try again.");
+      }
+    } else {
+      toast.error("Please provide both OTP and email.");
+    }
+  }
 
   return (
     <Fragment>
       <NavBar />
       <Container className={styles.FormContainer}>
-        <Form className={styles.TheForm}>
+        <Form onSubmit={handleSubmit} className={styles.TheForm}>
           <Form.Group className="mb-3">
-            <Form.Label className={styles.FormLabel}>Enter OTP code below:</Form.Label>
-            <Form.Control 
-            className={styles.FormField} 
-            type="text"
-            name="otp"
-            value={otp}
-            onChange={(e) => setOtp(e.target.value)}
+            <Form.Label className={styles.FormLabel}>Enter your email:</Form.Label>
+            <Form.Control
+              className={styles.FormField}
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label className={styles.FormLabel}>Enter your OTP code:</Form.Label>
+            <Form.Control
+              className={styles.FormField}
+              type="text"
+              name="otp"
+              value={otp}
+              onChange={(e) => setOtp(e.target.value)}
             />
           </Form.Group>
           <Button variant="primary" type="submit" className={styles.FormButton}>
