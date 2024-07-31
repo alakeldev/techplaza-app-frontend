d41e50bba8c576389af42607f2b7621f8d20fce6
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-07-31 02:11:47 +0200

    add basic structure to all 'auth dir .js files' and Profile.js + edit app.js add the Routes to these components and set its path for each one

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.module.css';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
-import './App.module.css';
+import LoginForm from './pages/auth/LoginForm';
+import RegisterForm from './pages/auth/RegisterForm';
+import EmailVerify from './pages/auth/EmailVerify';
+import ForgetPasswordForm from './pages/auth/ForgetPasswordForm';
+import Profile from './pages/profiles/Profile';
 
 function App() {
   return (
@@ -16,6 +21,11 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/otp/verify" element={<EmailVerify />} />
+          <Route path="/forget_password" element={<ForgetPasswordForm />} />
+          <Route path="/dashboard" element={<Profile />} />
         </Routes>
       </Router>
     </Fragment>
