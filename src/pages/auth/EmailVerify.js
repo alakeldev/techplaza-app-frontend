e6f927373b55327e9365d1d7c2e83d285f067cb5
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-07-31 13:32:38 +0200

    edit Emailverify.js inside auth 'call navbar and footer component + create the form to submit the otp' + creaete EmailVerify.module.css inside styles to set styles to the related component

--- src/pages/auth/EmailVerify.js
+++ src/pages/auth/EmailVerify.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Form, Button, Container } from 'react-bootstrap';
+import NavBar from '../../components/NavBar';
+import Footer from '../../components/Footer';
+import styles from '../../styles/EmailVerify.module.css';
 
 const EmailVerify = () => {
   return (
-    <div>EmailVerify</div>
+    <Fragment>
+      <NavBar />
+      <Container className={styles.FormContainer}>
+        <Form className={styles.TheForm}>
+          <Form.Group className="mb-3">
+            <Form.Label className={styles.FormLabel}>Enter OTP code below:</Form.Label>
+            <Form.Control className={styles.FormField} type="text" required />
+          </Form.Group>
+          <Button variant="primary" type="submit" className={styles.FormButton}>
+            Send
+          </Button>
+        </Form>
+      </Container>
+      <Footer />
+    </Fragment>
   )
 }
 
