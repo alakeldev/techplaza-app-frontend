ac97b4204da8a58ebb9423469c4face7fb42f7ab
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-07-31 22:11:56 +0200

    edit EmailVerify.js 'import useState + code tidy inside the form + add name, value, onChange and set its values related to OTP Form.Control'

--- src/pages/auth/EmailVerify.js
+++ src/pages/auth/EmailVerify.js
@@ -1,10 +1,12 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import { Form, Button, Container } from 'react-bootstrap';
 import NavBar from '../../components/NavBar';
 import Footer from '../../components/Footer';
 import styles from '../../styles/EmailVerify.module.css';
 
 const EmailVerify = () => {
+  const [otp, setOtp] = useState("")
+
   return (
     <Fragment>
       <NavBar />
@@ -12,7 +14,13 @@ const EmailVerify = () => {
         <Form className={styles.TheForm}>
           <Form.Group className="mb-3">
             <Form.Label className={styles.FormLabel}>Enter OTP code below:</Form.Label>
-            <Form.Control className={styles.FormField} type="text" required />
+            <Form.Control 
+            className={styles.FormField} 
+            type="text"
+            name="otp"
+            value={otp}
+            onChange={(e) => setOtp(e.target.value)}
+            />
           </Form.Group>
           <Button variant="primary" type="submit" className={styles.FormButton}>
             Send
