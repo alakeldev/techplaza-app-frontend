f56494267885be4f317dace69978f02dcdd5ed0a
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-01 00:12:16 +0200

    edit LoginForm.js auth dir 'import useState + create handleOnChange + add name,value, onChange to the Email and password fields Form.Control'

--- src/pages/auth/LoginForm.js
+++ src/pages/auth/LoginForm.js
@@ -1,10 +1,20 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react';
 import { Form, Button, Container } from 'react-bootstrap';
 import NavBar from '../../components/NavBar';
 import Footer from '../../components/Footer';
 import styles from '../../styles/LoginForm.module.css';
 
 const LoginForm = () => {
+  
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  })
+
+  const handleOnChange = (e) => {
+    setLoginData({...loginData, [e.target.name]: e.target.value})
+  }
+
   return (
     <Fragment>
       <NavBar />
@@ -13,12 +23,27 @@ const LoginForm = () => {
           <h2>Login</h2>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label className={styles.FormLabel}>Enter Your Email Address</Form.Label>
-            <Form.Control className={styles.FormField} type="email" placeholder="Enter your registered email" />
+            <Form.Control 
+            className={styles.FormField} 
+            type="email" 
+            placeholder="Enter your registered email" 
+            name="email"
+            value={loginData.email}
+            onChange={handleOnChange}
+            />
+            
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Enter Your Password</Form.Label>
-            <Form.Control className={styles.FormField} type="password" placeholder="Enter your password" />
+            <Form.Control 
+            className={styles.FormField} 
+            type="password" 
+            placeholder="Enter your password" 
+            name="password"
+            value={loginData.password}
+            onChange={handleOnChange}
+            />
           </Form.Group>
           <Button variant="primary" type="submit" className={styles.FormButton}>
             Login
