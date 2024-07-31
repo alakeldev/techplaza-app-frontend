300c49c0461e39d7b5ab3224e6add2981e189ae0
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-01 01:22:35 +0200

    create empty Dashboard.module.css to set styles to Dashboard.js in dashboard dir + edit Dashboard.js 'import useEffect & useNavigate & its css styles file control the dashboard page to show if it's use and jwt_token is valid or naviagte to login page + set a basic structure inside the return Fragment element'

--- src/pages/dashboards/Dashboard.js
+++ src/pages/dashboards/Dashboard.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { Fragment, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+import styles from '../../styles/Dashboard.module.css';
+
 
 const Dashboard = () => {
+  const navigate = useNavigate()
+  const user = JSON.parse(localStorage.getItem('user'))
+  const jwt_token = localStorage.getItem('access')
+
+
+  useEffect(() => {
+    if(jwt_token === null && !user) {
+      navigate("/login")
+    }
+  })
+
   return (
-    <div>Dashboard</div>
+    <Fragment>
+      <div>
+        <h2>Hi {user && user.name} </h2>
+        <h5>Welcome to Your Dashboard</h5>
+      </div>
+    </Fragment>
   )
 }
 
