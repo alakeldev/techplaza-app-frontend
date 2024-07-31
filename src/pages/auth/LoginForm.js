013233f4868ee5a0760f708bb6eaa76d55c51878
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-07-31 13:49:04 +0200

    edit LoginForm.js auth dir 'create login form + call navbar and footer components inside it' + create is css styles file inside styles dir 'LoginForm.module.css' and set its styles

--- src/pages/auth/LoginForm.js
+++ src/pages/auth/LoginForm.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Form, Button, Container } from 'react-bootstrap';
+import NavBar from '../../components/NavBar';
+import Footer from '../../components/Footer';
+import styles from '../../styles/LoginForm.module.css';
 
 const LoginForm = () => {
   return (
-    <div>LoginForm</div>
+    <Fragment>
+      <NavBar />
+      <Container className={styles.FormContainer}>
+        <Form className={styles.TheForm}>
+          <h2>Login</h2>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label className={styles.FormLabel}>Enter Your Email Address</Form.Label>
+            <Form.Control className={styles.FormField} type="email" placeholder="Enter your registered email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Enter Your Password</Form.Label>
+            <Form.Control className={styles.FormField} type="password" placeholder="Enter your password" />
+          </Form.Group>
+          <Button variant="primary" type="submit" className={styles.FormButton}>
+            Login
+          </Button>
+        </Form>
+      </Container>
+      <Footer />
+    </Fragment>
   )
 }
 
