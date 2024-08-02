9e90087ccd17d3a2bcd7bf5c0d997f31cd2cbab1
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-02 18:46:08 +0200

    edit NavBar.js 'import useNavigate from router-dom + create handlelogout to naviate to logout-confirmation path + edit in return structure in Navbar to show and hide link if user loggedin or not'

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -7,6 +7,13 @@ import styles from "../styles/NavBar.module.css";
 import Logo from "../assets/logo.png";
 
 const NavBar = () => {
+  const navigate = useNavigate();
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  const handleLogout = () => {
+    navigate("/logout-confirmation");
+  };
+
   return (
     <Navbar expand="lg" className={styles.Nav} fixed="top">
       <Container>
@@ -20,8 +27,17 @@ const NavBar = () => {
             <Nav.Link as={Link} to="/about" className={`${styles.Nav_Anchor_Tag}`}><i className="fa-solid fa-circle-info"></i> About</Nav.Link>
             <Nav.Link as={Link} to="/services" className={`${styles.Nav_Anchor_Tag}`}><i className="fa-solid fa-gear"></i> Services</Nav.Link>
             <Nav.Link as={Link} to="/contact" className={`${styles.Nav_Anchor_Tag}`}><i className="fa-solid fa-paper-plane"></i> Contact</Nav.Link>
-            <Nav.Link as={Link} to="/login" className={`${styles.Nav_Anchor_Tag}`}><i className="fa-solid fa-arrow-right-to-bracket"></i> Login</Nav.Link>
-            <Nav.Link as={Link} to="/register" className={`${styles.Nav_Anchor_Tag}`}><button className={styles.NavButton}><i className="fa-solid fa-user-plus"></i> Register</button></Nav.Link>
+            {user ? (
+              <>
+              <Nav.Link as={Link} to="/dashboard" className={`${styles.Nav_Anchor_Tag}`}><i className="fa-solid fa-tachometer-alt"></i> Dashboard</Nav.Link>
+              <Nav.Link onClick={handleLogout} className={`${styles.Nav_Anchor_Tag}`}><i className="fa-solid fa-sign-out-alt"></i> Logout</Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={Link} to="/login" className={`${styles.Nav_Anchor_Tag}`}><i className="fa-solid fa-arrow-right-to-bracket"></i> Login</Nav.Link>
+                <Nav.Link as={Link} to="/register" className={`${styles.Nav_Anchor_Tag}`}><button className={styles.NavButton}><i className="fa-solid fa-user-plus"></i> Register</button></Nav.Link>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
---
+++ src/pages/auth/LogoutConfirmation.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LogoutConfirmation = () => {
+  return (
+    <div>LogoutConfirmation</div>
+  )
+}
+
+export default LogoutConfirmation
