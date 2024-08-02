c30f48cf874bca46407c71e5090b3df1259b7e7e
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-02 14:32:41 +0200

    edit Dashboard.js in dashboards dir 'import Navbar and Footer components + edit in the basic return structure'

--- src/pages/dashboards/Dashboard.js
+++ src/pages/dashboards/Dashboard.js
@@ -1,5 +1,7 @@
 import React, { Fragment, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
+import NavBar from '../../components/NavBar';
+import Footer from '../../components/Footer';
 import styles from '../../styles/Dashboard.module.css';
 
 
@@ -13,14 +15,27 @@ const Dashboard = () => {
     if(jwt_token === null && !user) {
       navigate("/login")
     }
-  })
+  }, [jwt_token, user, navigate]);
 
   return (
     <Fragment>
+      <NavBar />
       <div>
-        <h2>Hi {user && user.name} </h2>
+        <h2>Hi {user && user.name}</h2>
         <h5>Welcome to Your Dashboard</h5>
+        <button>Edit my information</button>
+        <button>Logout</button>
+        <button>Delete Account</button>
+        <div>
+          <div>
+            <h3>Application 1</h3>
+          </div>
+          <div>
+            <h3>Application 2</h3>
+          </div>
+        </div>
       </div>
+      <Footer />
     </Fragment>
   )
 }
