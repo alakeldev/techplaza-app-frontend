eb9f4e9ec03f58ebe4312fc2988237ba15e788b3
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-05 23:08:13 +0200

    edit WeatherApp.js 'remove apiKey from the file and store it in the backend + useState to set errors scenarios and display it below the search button + edit toGetWeather: set conditions and use regular expression to validate the weather input + fetch to connect with backend path and check the api response and display the json data or the error and clear the input

--- src/pages/weatherapp/WeatherApp.js
+++ src/pages/weatherapp/WeatherApp.js
@@ -6,8 +6,8 @@ import styles from '../../styles/WeatherApp.module.css';
 
 const WeatherApp = () => {
   const [city, setCity] = useState("");
-  const apiKey = '1fb0e0f6964d49c9eebe77c11137bb6c';
   const [weather, setWeather] = useState({});
+  const [error, setError] = useState("");
   const navigate = useNavigate();
   const user = JSON.parse(localStorage.getItem('user'));
   const jwt_token = localStorage.getItem('access');
@@ -19,12 +19,32 @@ const WeatherApp = () => {
   }, [jwt_token, user, navigate]);
 
   const toGetWeather = (e) => {
-    if (e.key === 'Enter') {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-        .then(res => res.json())
+    if (e.key === 'Enter' || e.type === 'click') {
+      const cityRegex = /^[a-zA-Z\s]+$/;
+      if (!cityRegex.test(city)) {
+        setError('Invalid city name. Please enter letters only.');
+        setWeather({});
+        return;
+      }
+
+      fetch(`http://127.0.0.1:8000/api/weather?city=${city}`)
+        .then(res => {
+          if (!res.ok) {
+            throw new Error('Invalid city name. Please enter a valid city.');
+          }
+          return res.json();
+        })
         .then(result => {
+          if (result.cod === '404') {
+            throw new Error('City not found. Please enter a valid city name.');
+          }
           setWeather(result);
-          setCity('')
+          setCity('');
+          setError('');
+        })
+        .catch(error => {
+          setError(error.message);
+          setWeather({});
         })
     }
   }
@@ -36,12 +56,13 @@ const WeatherApp = () => {
           <h2>Weather Application</h2>
           <input
             type='text'
-            placeholder='Search by City...'
+            placeholder='Search by City or Country...'
             value={city}
             onChange={(e) => setCity(e.target.value)}
             onKeyDown={toGetWeather}
           />
           <button onClick={toGetWeather}>Search</button>
+          {error && <div className={styles.Error}>{error}</div>}
           <div className={styles.WeatherDetails}>
             <div className={styles.CityLocation}>
               {weather.name} {weather.sys && weather.sys.country}
@@ -63,4 +84,4 @@ const WeatherApp = () => {
   );
 };
 
-export default WeatherApp
+export default WeatherApp;
