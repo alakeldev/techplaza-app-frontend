ed0f94fb6b74327a6993ab55359393eadede2644
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-06 20:44:00 +0200

    edit WeatherApp.js 'edit and set the new backend path in fetch()'

--- src/pages/weatherapp/WeatherApp.js
+++ src/pages/weatherapp/WeatherApp.js
@@ -27,7 +27,7 @@ const WeatherApp = () => {
         return;
       }
 
-      fetch(`http://127.0.0.1:8000/api/weather?city=${city}`)
+      fetch(`http://127.0.0.1:8000/api/app1/weather?city=${city}`)
         .then(res => {
           if (!res.ok) {
             throw new Error('Invalid city name. Please enter a valid city.');
