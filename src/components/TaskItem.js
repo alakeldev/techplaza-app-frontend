c11a27851b10b750d89ddabd8a7df43c2bb74e59
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-08 13:38:58 +0200

    craete TaskItem.js inside components dir 'related to tasksapp'

---
+++ src/components/TaskItem.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button, ListGroupItem } from 'react-bootstrap';
+
+const TaskItem = ({ item, editItem, handleDelete, viewDone }) => {
+    return (
+        <ListGroupItem
+            key={item.id}
+            className="d-flex justify-content-between align-items-center"
+        >
+            <span
+                className={`mr-2 ${viewDone ? "text-decoration-line-through" : ""}`}
+                title={item.description}
+            >
+                {item.title}
+            </span>
+            <span>
+                <Button
+                    onClick={() => editItem(item)}
+                    variant="secondary"
+                    className="mr-2"
+                >
+                    Edit
+                </Button>
+                <Button
+                    onClick={() => handleDelete(item)}
+                    variant="danger"
+                >
+                    Delete
+                </Button>
+            </span>
+        </ListGroupItem>
+    );
+};
+
+export default TaskItem;
