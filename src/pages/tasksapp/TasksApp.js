af39b181a422e93dc891f7a4d8b3473fd89a6a37
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-08 14:25:32 +0200

    edit TasksApp.js 'import different components from react-bootstrap + import TaskList & TaskModal & TabList components from components dir'

--- src/pages/tasksapp/TasksApp.js
+++ src/pages/tasksapp/TasksApp.js
@@ -1,8 +1,12 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { Button, Card, Container, Row, Col } from 'react-bootstrap';
 import NavBar from '../../components/NavBar';
 import Footer from '../../components/Footer';
+import TaskList from '../../components/TaskList';
+import TaskModal from '../../components/TaskModal';
+import TabList from '../../components/TabList';
 import styles from '../../styles/TasksApp.module.css';
 
 const TasksApp = () => {
