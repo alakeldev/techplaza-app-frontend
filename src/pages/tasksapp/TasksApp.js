41bd41d0fdc74e1f85a66c5dc83e87ec70aa14e6
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-08 18:01:19 +0200

    edit TasksApp.js 'use react-boostrap with return structure statement + useState and useeffect hook again + set state veriables + create utility function refreshList() + create state management functions displayDone, toggle, handleChange + create CRUD function perations handleSubmit() + handleDelete() + createItem() + editItem() + use axios with delete,post,put,get with backend paths'

--- src/pages/tasksapp/TasksApp.js
+++ src/pages/tasksapp/TasksApp.js
@@ -21,14 +21,106 @@ const TasksApp = () => {
         }
     }, [jwt_token, user, navigate]);
 
+    const [viewDone, setViewDone] = useState(false);
+    const [activeItem, setActiveItem] = useState({
+        task_title: "",
+        task_description: "",
+        is_done: false
+    });
+    const [taskList, setTaskList] = useState([]);
+    const [modal, setModal] = useState(false);
 
+    useEffect(() => {
+        refreshList();
+    }, []);
 
+    const refreshList = () => {
+        axios
+            .get("http://127.0.0.1:8000/api/app2/tasks_manager/")
+            .then(res => setTaskList(res.data))
+            .catch(err => console.log(err));
+    };
+
+    const displayDone = status => {
+        setViewDone(status);
+    };
+
+    const toggle = () => {
+        setModal(!modal);
+    };
+
+    const handleChange = e => {
+        let { name, value } = e.target;
+        if (e.target.type === "checkbox") {
+            value = e.target.checked;
+        }
+        setActiveItem({ ...activeItem, [name]: value });
+    };
+
+    const handleSubmit = item => {
+        toggle();
+        console.log("Submitting item:", item);
+        if (item.id) {
+            axios
+                .put(`http://127.0.0.1:8000/api/app2/tasks_manager/${item.id}/`, item)
+                .then(res => refreshList())
+                .catch(err => console.log(err.response));
+            return;
+        }
+        axios
+            .post("http://127.0.0.1:8000/api/app2/tasks_manager/", item)
+            .then(res => refreshList())
+            .catch(err => console.log(err.response));
+    };
+
+    const handleDelete = item => {
+        axios
+            .delete(`http://127.0.0.1:8000/api/app2/tasks_manager/${item.id}/`)
+            .then(res => refreshList())
+            .catch(err => console.log(err.response));
+    };
+
+    const createItem = () => {
+        const item = { task_title: "", task_description: "", is_done: false };
+        setActiveItem(item);
+        setModal(!modal);
+    };
+
+    const editItem = item => {
+        setActiveItem(item);
+        setModal(!modal);
+    };
 
     return (
         <Fragment>
-            <NavBar/>
-            <div></div>
-            <Footer/>
+            <NavBar />
+            <Container className="content">
+                <h1 className="text-black text-uppercase text-center my-4">Task Manager</h1>
+                <Row className="justify-content-center">
+                    <Col md={6} sm={10}>
+                        <Card className="p-3">
+                            <Button onClick={createItem} variant="primary" className="mb-3">
+                                Add task
+                            </Button>
+                            <TabList viewDone={viewDone} displayDone={displayDone} />
+                            <TaskList
+                                taskList={taskList}
+                                viewDone={viewDone}
+                                editItem={editItem}
+                                handleDelete={handleDelete}
+                            />
+                        </Card>
+                    </Col>
+                </Row>
+                <TaskModal
+                    modal={modal}
+                    toggle={toggle}
+                    activeItem={activeItem}
+                    handleChange={handleChange}
+                    handleSubmit={handleSubmit}
+                />
+            </Container>
+            <Footer />
         </Fragment>
     )
 }
