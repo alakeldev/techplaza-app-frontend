5fd1da35f67e6e6619931e042b8d9c153bb26876
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-09 21:27:53 +0200

    create snake game components with its own css style file for each 'Snakegame.module.css + SnakeGameBoard.js & its own module.css file + SnakeGameHighScores.js & its own module.css file'

---
+++ src/components/SnakeGameBoard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SnakeGameBoard = () => {
+  return (
+    <div>SnakeGameBoard</div>
+  )
+}
+
+export default SnakeGameBoard
---
+++ src/components/SnakeGameHighScores.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SnakeGameHighScores = () => {
+  return (
+    <div>SnakeGameHighScores</div>
+  )
+}
+
+export default SnakeGameHighScores
