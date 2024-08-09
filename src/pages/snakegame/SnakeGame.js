255223c1b9f242c1ff196edc3eddf982e1d93adf
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-10 01:37:04 +0200

    edit SnakeGame.js 'import NavBar, Footer, SnakeGameBoard components, useeffect hooks to prevent unlogged user to reach to this app + use react-bootstrap within return statement strucutre'

--- src/pages/snakegame/SnakeGame.js
+++ src/pages/snakegame/SnakeGame.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { Fragment, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Card, Container, Row, Col } from 'react-bootstrap';
+import NavBar from '../../components/NavBar';
+import Footer from '../../components/Footer';
+import SnakeGameBoard from '../../components/SnakeGameBoard';
+import styles from '../../styles/SnakeGame.module.css';
+
 
 const SnakeGame = () => {
-  return (
-    <div>SnakeGame</div>
-  )
-}
 
-export default SnakeGame
+  const user = JSON.parse(localStorage.getItem('user'));
+  const jwt_token = localStorage.getItem('access');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (jwt_token === null && !user) {
+      navigate("/login");
+    }
+  }, [jwt_token, user, navigate]);
+
+  return (
+    <Fragment>
+      <NavBar />
+      <Container className="content">
+        <h1 className="text-black text-uppercase text-center my-4">Welcome To Snake Game</h1>
+        <Row className="justify-content-center">
+          <Col md={6} sm={10}>
+            <Card className="p-3">
+              <SnakeGameBoard />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default SnakeGame;
