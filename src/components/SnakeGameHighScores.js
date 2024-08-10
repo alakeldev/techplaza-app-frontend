c63ceb8889a8fe4d258ea7bd0b55fa20d873d3c2
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-10 14:49:12 +0200

    edit SnakeGamesHighScores.js 'edit and set elements to display top scores' + edit SnakeGameHighScores.module.css 'set basic css styles to the SnakeGameHightScore component'

--- src/components/SnakeGameHighScores.js
+++ src/components/SnakeGameHighScores.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styles/SnakeGameHighScores.module.css';
 
-const SnakeGameHighScores = () => {
+const SnakeGameHighScores = ({ highScores }) => {
   return (
-    <div>SnakeGameHighScores</div>
+    <div className={styles.highScores}>
+      <h3 className={styles.heading}>Top Scores</h3>
+      <ul className={styles.list}>
+        {highScores.map(score => (
+          <li key={score.id} className={styles.listItem}>
+            {score.user.full_name || score.user.email}: {score.score}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
