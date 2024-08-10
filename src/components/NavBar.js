8875e8652727d887a6ecf8654f3cc0cad88ee450
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-10 18:54:24 +0200

    edit NavBar.js 'import useLocation from react-router-dom + use it in each Nav.Link to set Active class on the active link/path' + edit NavBar.module.css 'added some styles + added media queries for small, med, larg..'

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -13,21 +13,22 @@ import { toast } from "react-toastify";
 const NavBar = () => {
   const user = JSON.parse(localStorage.getItem('user'));
   const refresh = JSON.parse(localStorage.getItem('refresh'));
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const location = useLocation();
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   const handleLogout = async () => {
-    const res = await axiosInstance.post("/auth/logout/", { "refresh_token": refresh })
+    const res = await axiosInstance.post("/auth/logout/", { "refresh_token": refresh });
     if (res.status === 200) {
-      localStorage.removeItem("user")
-      localStorage.removeItem("access")
-      localStorage.removeItem("refresh")
-      navigate("/")
-      toast.success("logout successful")
+      localStorage.removeItem("user");
+      localStorage.removeItem("access");
+      localStorage.removeItem("refresh");
+      navigate("/");
+      toast.success("logout successful");
     }
-  }
+  };
 
   return (
     <>
@@ -39,19 +40,19 @@ const NavBar = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className={styles.MarginNavLinks}>
-              <Nav.Link as={Link} to="/home" className={`${styles.Nav_Anchor_Tag}`}><i className="fa-solid fa-house"></i> Home</Nav.Link>
-              <Nav.Link as={Link} to="/about" className={`${styles.Nav_Anchor_Tag}`}><i className="fa-solid fa-circle-info"></i> About</Nav.Link>
-              <Nav.Link as={Link} to="/services" className={`${styles.Nav_Anchor_Tag}`}><i className="fa-solid fa-gear"></i> Services</Nav.Link>
-              <Nav.Link as={Link} to="/contact" className={`${styles.Nav_Anchor_Tag}`}><i className="fa-solid fa-paper-plane"></i> Contact</Nav.Link>
+              <Nav.Link as={Link} to="/home" className={`${styles.Nav_Anchor_Tag} ${(location.pathname === '/' || location.pathname === '/home') ? styles.Active : ''}`}><i className="fa-solid fa-house"></i> Home</Nav.Link>
+              <Nav.Link as={Link} to="/about" className={`${styles.Nav_Anchor_Tag} ${location.pathname === '/about' ? styles.Active : ''}`}><i className="fa-solid fa-circle-info"></i> About</Nav.Link>
+              <Nav.Link as={Link} to="/services" className={`${styles.Nav_Anchor_Tag} ${location.pathname === '/services' ? styles.Active : ''}`}><i className="fa-solid fa-gear"></i> Services</Nav.Link>
+              <Nav.Link as={Link} to="/contact" className={`${styles.Nav_Anchor_Tag} ${location.pathname === '/contact' ? styles.Active : ''}`}><i className="fa-solid fa-paper-plane"></i> Contact</Nav.Link>
               {user ? (
                 <>
-                  <Nav.Link as={Link} to="/dashboard" className={`${styles.Nav_Anchor_Tag}`}><i className="fa-solid fa-tachometer-alt"></i> Dashboard</Nav.Link>
+                  <Nav.Link as={Link} to="/dashboard" className={`${styles.Nav_Anchor_Tag} ${location.pathname === '/dashboard' ? styles.Active : ''}`}><i className="fa-solid fa-tachometer-alt"></i> Dashboard</Nav.Link>
                   <Nav.Link onClick={handleShow} className={`${styles.Nav_Anchor_Tag}`}><i className="fa-solid fa-sign-out-alt"></i> Logout</Nav.Link>
                 </>
               ) : (
                 <>
-                  <Nav.Link as={Link} to="/login" className={`${styles.Nav_Anchor_Tag}`}><i className="fa-solid fa-arrow-right-to-bracket"></i> Login</Nav.Link>
-                  <Nav.Link as={Link} to="/register" className={`${styles.Nav_Anchor_Tag}`}><button className={styles.NavButton}><i className="fa-solid fa-user-plus"></i> Register</button></Nav.Link>
+                  <Nav.Link as={Link} to="/login" className={`${styles.Nav_Anchor_Tag} ${location.pathname === '/login' ? styles.Active : ''}`}><i className="fa-solid fa-arrow-right-to-bracket"></i> Login</Nav.Link>
+                  <Nav.Link as={Link} to="/register" className={`${styles.Nav_Anchor_Tag} ${location.pathname === '/register' ? styles.Active : ''}`}><i className="fa-solid fa-user-plus"></i> Register</Nav.Link>
                 </>
               )}
             </Nav>
