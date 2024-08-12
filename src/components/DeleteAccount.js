98720434d72f8a5a416e57ec9bd417e826e930fe
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-12 19:46:21 +0200

    edit DeleteAccount.js 'create handleDelete fun and use axiosInstance to get response from the backend path or catch an error + added the modal from react-bostrap and set className to its elements' + edit DeleteAccount.module.css 'added css styles + media queries to the small and med screens'

--- src/components/DeleteAccount.js
+++ src/components/DeleteAccount.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from "react-toastify";
+import axiosInstance from '../utils/axiosInstance';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import styles from '../styles/DeleteAccount.module.css';
 
-const DeleteAccount = () => {
+const DeleteAccount = ({ show, handleClose }) => {
+
+  const navigate = useNavigate()
+  const handleDelete = async () => {
+    try {
+      const response = await axiosInstance.delete('/auth/delete-account/');
+      if (response.status === 204) {
+        localStorage.removeItem("user");
+        localStorage.removeItem("access");
+        localStorage.removeItem("refresh");
+        navigate("/");
+        toast.success('Account deleted successfully');
+      }
+    } catch (error) {
+      toast.error('Failed to delete account');
+    }
+  };
   return (
-    <div>DeleteAccount</div>
-  )
-}
+    <Modal show={show} onHide={handleClose} centered className={styles.modalDialog}>
+      <Modal.Header closeButton className={styles.modalHeader}>
+        <Modal.Title>Delete Account</Modal.Title>
+      </Modal.Header>
+      <Modal.Body className={styles.modalBody}>
+        Are you sure you want to delete your account?
+      </Modal.Body>
+      <Modal.Footer className={styles.modalFooter}>
+        <Button variant="secondary" onClick={handleClose} className={styles.btnSecondary}>
+          No
+        </Button>
+        <Button variant="danger" onClick={handleDelete} className={styles.btnPrimary}>
+          Yes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
 
-export default DeleteAccount
+export default DeleteAccount;
