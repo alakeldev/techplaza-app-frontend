9d5a87a2b340b459e4d56ebf845ccaf5ace19965
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-15 15:43:26 +0200

    edit CardsApp.js 'only loggedin user can access this app path using useEffect hook + use helmet for title + import NavBar and Footer components'

--- src/pages/cardsapp/CardsApp.js
+++ src/pages/cardsapp/CardsApp.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { Fragment, useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+import { useNavigate } from 'react-router-dom';
+import NavBar from '../../components/NavBar';
+import Footer from '../../components/Footer';
 
 const CardsApp = () => {
+
+  const user = JSON.parse(localStorage.getItem('user'));
+  const jwt_token = localStorage.getItem('access');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (jwt_token === null && !user) {
+      navigate("/login");
+    }
+  }, [jwt_token, user, navigate]);
+
+
+
   return (
-    <div>CardsApp</div>
+    <Fragment>
+      <Helmet>
+        <title>Cards Members - TechPlaza Platform</title>
+      </Helmet>
+      <NavBar />
+
+      <Footer />
+    </Fragment>
   )
 }
 
